8a426708d9d35eba3c302583826366c56627f95b
John Cotter <john.cotter@gmail.com>
2022-04-08 10:42:00 -0300

    Fix for Storybook not using Chakra styling

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,18 @@
 module.exports = {
+  refs: {
+    // Remove chakra ui from showing up along side our components
+    '@chakra-ui/react': {
+      disable: true,
+    },
+  },
   stories: [
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-interactions'],
+  addons: [
+    '@chakra-ui/storybook-addon',
+    '@storybook/addon-links',
+    '@storybook/addon-interactions',
+  ],
   framework: '@storybook/react',
 };
